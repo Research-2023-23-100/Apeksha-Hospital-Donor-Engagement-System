d2edd37d4f57a9ff7dbd49254acaf0f012b54a63
Sahan Punchihewa <sahannilupul18@gmail.com>
2024-03-09 09:23:30 +0530

    Donor Register and Login Completed

---
+++ backend/src/api/controllers/Donor.controller.js
@@ -0,0 +1,70 @@
+import DonorModel from "../models/Donor.model";
+import DonorService from "../services";
+import logger from "../../util/logger";
+
+// Donor Login
+export const loginDonor = async (request, response, next) => {
+	const { email, password } = request.body;
+
+	if (email && password) {
+		await DonorService.authenticateDonor(email, password)
+			.then(async (user) => {
+				const authToken = await user.generateAuthToken();
+				const data = {
+					_id: user._id,
+					name: user.name,
+					email: user.email,
+					token: authToken,
+					permissionLevel: user.permissionLevel,
+				};
+
+				request.handleResponse.successRespond(response)(data);
+			})
+			.catch((error) => {
+				const errorResponseData = {
+					errorTime: new Date(),
+					message: error.message,
+				};
+
+				logger.error(JSON.stringify(errorResponseData));
+				request.handleResponse.errorRespond(response)(errorResponseData);
+				next();
+			});
+	} else {
+		logger.error("Username or Password is missing");
+		request.handleResponse.errorRespond(response)("Username or Password is missing");
+		next();
+	}
+};
+
+
+
+// Donor register
+export const registerDonor = async (request, response, next) => {
+	if (await DonorModel.findOne({ email: request.body.email })) {
+		request.handleResponse.errorRespond(response)("Email already exists");
+		next();
+	} else if (await DonorModel.findOne({ nic: request.body.nic })) {
+		request.handleResponse.errorRespond(response)("NIC already exists");
+	} else {
+		const Staff = {
+			name: request.body.name,
+			email: request.body.email,
+            nic: request.body.nic,
+			password: request.body.password,
+			permissionLevel: "DONOR",
+		};
+
+		await DonorService.insertDonor(Staff)
+			.then((data) => {
+				logger.info(`New User with ID ${data._id} created`);
+				request.handleResponse.successRespond(response)(data);
+				next();
+			})
+			.catch((error) => {
+				logger.error(error.message);
+				request.handleResponse.errorRespond(response)(error.message);
+				next();
+			});
+	}
+};
---
+++ backend/src/api/services/Donor.service.js
@@ -0,0 +1,72 @@
+import DonorModel from "../models/Donor.model";
+
+// Authenticate Donor
+export const authenticateDonor = async (email, password) => {
+	return await DonorModel.findOne({ email })
+		.then(async (user) => {
+			if (user && (await user.matchPassword(password))) {
+				return user;
+			} else {
+				throw new Error("Invalid Email or Password!");
+			}
+		})
+		.catch((error) => {
+			throw new Error(error.message);
+		});
+};
+
+// Insert Donor
+export const insertDonor = async (user) => {
+	return await DonorModel.create(user)
+		.then(async (user) => {
+			await user.generateAuthToken();
+			return user;
+		})
+		.catch((error) => {
+			throw new Error(error.message);
+		});
+};
+
+// Get one Donor Details
+export const getDonorDetails = async (userId) => {
+	return await DonorModel.findById(userId)
+		.then((user) => {
+			return user;
+		})
+		.catch((error) => {
+			throw new Error(error.message);
+		});
+};
+
+// Get All Donor Details
+export const getAllDonor = async () => {
+	return await DonorModel.find()
+		.then((user) => {
+			return user;
+		})
+		.catch((error) => {
+			throw new Error(error.message);
+		});
+};
+
+// Update Donor Details
+export const editDonorDetails = async (userId, Staff) => {
+	return await DonorModel.findByIdAndUpdate(userId, Staff, { new: true })
+		.then((Staff) => {
+			return Staff;
+		})
+		.catch((error) => {
+			throw new Error(error.message);
+		});
+};
+
+// Delete Donor user
+export const deleteDonor = async (userId) => {
+	return await DonorModel.findByIdAndDelete(userId)
+		.then((user) => {
+			return user;
+		})
+		.catch((error) => {
+			throw new Error(error.message);
+		});
+};

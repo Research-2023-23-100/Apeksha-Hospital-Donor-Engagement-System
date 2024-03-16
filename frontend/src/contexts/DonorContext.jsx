413b7e43bf1a99852ca1afdc387c43862be42734
Sahan Punchihewa <sahannilupul18@gmail.com>
2024-03-17 01:57:31 +0530

    Header Change

--- frontend/src/contexts/DonorContext.jsx
+++ frontend/src/contexts/DonorContext.jsx
@@ -9,6 +9,22 @@ export function DonorProvider({ children }) {
 	const [donor, setDonor] = useState([]);
 	const [donors, setDonors] = useState({});
 
+	const login = (values) => {
+		DonorAPI.login(values)
+			.then((response) => {
+				localStorage.setItem("uId", response.data._id);
+				localStorage.setItem("name", response.data.name);
+				localStorage.setItem("email", response.data.email);
+				localStorage.setItem("authToken", response.data.token);
+				localStorage.setItem("permissionLevel", response.data.permissionLevel);
+				makeToast({ type: "success", message: "Login Successful" });
+				window.location.href = "/staff";
+			})
+			.catch((err) => {
+				makeToast({ type: "error", message: "Invalid Email or Password" });
+			});
+	};
+
 	return (
 		<DonorContext.Provider
 			value={{
@@ -16,6 +32,7 @@ export function DonorProvider({ children }) {
 				setDonor,
 				donors,
 				setDonors,
+				login,
 			}}
 		>
 			{children}
